7f94ab2359af1f17984b911e74763047e7153aac
narendragolla1 <116960868+narendragolla1@users.noreply.github.com>
2023-06-01 15:24:06 +0530

    JavaScript

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+
+->fucntional programming language and lambda langauge
+->loosely and dynamically typed: 
+Eg: var a=5;
+    we dont write int,float,char that is why we call it as loosely typed language
+    var a="hello";
+    we have change the data type of the a variable when we compiled it will dyanmically change the 
+    the datatype to string.
+     
+
+Data types
+    1. Number
+    2. string
+    3. Boolean
+    4 Undefined
+    5.Null
+
+
+    Eg: var a=10;
+    var b="hello";
+    var c=true;
+
+    Null in js means there is no value.
+    we have to assign NUll value to it::: var null_vari=null;
+    Undefined in js means we didn't define value.
+
+
+
+*/
+var a = 10;
+var b = "hello";
+var c = true;
+console.log(a);
+console.log(b);
+console.log(c);
